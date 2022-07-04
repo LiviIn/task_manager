c7e1feebc610758a51ace1db63c381a308cd4b56
Livin R <liviin651@gmail.com>
2022-07-04 15:08:33 +0530

    set a rest api

--- node_task/app.js
+++ node_task/app.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+const { mongoose } = require('./db/mongoose');
+
+const bodyParser = require('body-parser')
+
+// Load in the mongoose models
+const { List, Task } = require('./db/models')
+
+// Load middleware
+app.use(bodyParser.json());
+
 /* ROUTE HANDLERS */
 
 
@@ -13,7 +23,9 @@ const app = express();
  */
 app.get('/lists', (req, res) =>{
     // we want to return an array of all lists in the database
-    res.send('Hello world')
+    List.find({}).then((lists) => {
+        res.send(lists);
+    })
 })
 
 /**
@@ -23,6 +35,15 @@ app.get('/lists', (req, res) =>{
 app.post ('/lists', (req, res) => {
     // we want to create a new list and return the new list document back to the user (which includes the id)
     // the list information (fields) will be passed in via the JSON request body
+    let title = req.body.title;
+
+    let newList = new List({
+        title
+    });
+    newList.save().then((listDoc) => {
+        // the full list document is returned(incl .id)
+        res.send(listDoc)
+    })
 })
 
 /**
@@ -31,6 +52,12 @@ app.post ('/lists', (req, res) => {
  */
 app.patch('lists/:id', (req, res) =>{
     // we want to update the specified list (list document with id in the URL) with the new value specified in the JSON body of the request
+
+    List.findOneAndUpdate({
+        _id: req.params.id
+    }, { $set: req.body }).then(() =>{
+        res.sendStatus(200);
+    })
 })
 
 /**
@@ -39,8 +66,77 @@ app.patch('lists/:id', (req, res) =>{
  */
 app.delete('lists/:id', (req, res) =>{
     // We want to delete the specified list (document with id in the URL)
+    List.findOneAndRemove({
+        _id: req.params.id
+    }).then((removeListDoc) =>{
+        res.send(removeListDoc)
+    })
 })
 
+/**
+ * GET /lists/:listId/tasks
+ * Purpose: Get all tasks that belong to a specific list (specified by listId )
+ */
+app.get('/lists/:listId/tasks', (req, res) =>{
+    // we want to remove all task that to a specific list (specified by listed)
+    Task.find({
+        _listId: req.params.listID
+    }).then((tasks) =>{
+        res.send(tasks);
+    })
+})
+
+app.get('/lists/:listId/tasks/:taskId', (req, res) =>{
+    Task.findOne({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }).then((task) =>{
+        res.send(task);
+    })
+})
+
+
+/**
+ * POST /lists/:listId/tasks
+ * Purpose: Create a new task in a list specified by listId
+ */
+app.post('/lists/:listId/tasks', (req, res) => {
+    // We want to create a new task in a list specified by listId
+    let nreTask = new Task({
+        title: req.body.title,
+        _listId: req.params.listId
+    });
+    newTask.save().then((newTaskDoc) =>{
+        res.send(newTaskDoc);
+    })
+})
+
+/**
+ * PATCH /lists/:listId/tasks/:taskId
+ * Purpose: Update an existing task
+ */
+app.patch('/lists/:listId/tasks/:taskId', (req, res) =>{
+    // We want to update an existing task (specified by taskId)
+    Task.findOneAndUpdate({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }, { $set: req.body}).then(() =>{
+        res.sendStatus(200);
+    })
+})
+
+/**
+ * DELETE /lists/:listId/tasks/:taskId
+ * Purpose: Delete a task
+ */
+app.delete('/lists/:listId/tasks/:taskId', (req, res) =>{
+    Task.findOneAndRemove({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }).then((removedTaskDoc) =>{
+        res.send(removedTaskDoc)
+    })
+})
 app.listen(3000, () =>{
     console.log('Server is listening on port 3000')
 })
--- node_task/db/mongoose.js
+++ node_task/db/mongoose.js
@@ -2,3 +2,14 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TaskManager', {
+    useNewUrlParser: true
+}).then(() =>{
+    console.log('connected to mongoDB successfully');
+}).catch((err) =>{
+    console.log('Error while attempting to connect to mongoDB', err)
+});
+
+module.exports = {
+    mongoose
+}
